97efaf4cdc519780530241a4907f18eed49c98ac
enzhdanova <listochek.katerinskiy@gmail.com>
2022-10-14 12:19:39 +0700

    add test tabs

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.fooddelivery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import com.example.fooddelivery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(R.id.fragment, FoodMenuFragment(), FoodMenuFragment.javaClass.simpleName)
+        }
     }
 }
